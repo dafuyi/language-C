72a24a25cfb019d913b68d00e71c8f363feba0ad
dafuyi <1427019390@qq.com>
2017-06-22 09:56:55 +0800

    V1 完成了计算 X 坐标，tmp 是 X 坐标的 1/100

--- demo2-17-6-22.c
+++ demo2-17-6-22.c
@@ -5,19 +5,19 @@
 
 #include <stdio.h>
 #include <math.h>
-
 /**
  *
  * @function 这个函数是用来用 * 绘制余弦函数的
  * @return 0
  */
 int main() {
-//    int length = 100;
-    double angle = 0.0;
-    double tmp = 0;
-    for(int i = 0 ; i <= 180 ;  i += 20 ) {
-        tmp = cos(angle + i);
-        printf("%.5f\n",tmp);
+    double length = 0,
+            PI = 3.1415;
+    for(int i = 1 ; i <= 20 ;  i ++ ) {
+        length = cos(PI/2 * 0.05 * i)* 100;
+        for(int j = 0 ; j < length ; j += 3)
+            printf(" ");
+        printf("*\n");
     }
     return 0;
 }
